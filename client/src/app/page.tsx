7e692a37afce6c121044e5de791382859b5f81db
Bayra1 <isvhbaatar5@gmail.com>
2024-07-16 16:37:11 +0800

    test

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,16 @@
 import { DebounceTest } from "@/components";
+import { BoardForDinosour } from "@/components/Dinosaur/Board";
 import { Three_D_Cube } from "@/features";
 import { DebounceTestFeature } from "@/features/DebounceTest";
 import { Sign_Up } from "@/features/Sign_Up";
 
 export default function Home() {
   return (
-    <div className="w-[100%] flex justify-center items-center h-full mt-[250px]">
+    <div className="w-screen flex justify-center items-center h-screen mt-[250px] p-4 bg-gray-700">
       {/* <button className="mt-[150px] ml-[150px] border p-[10px]">click me </button> */}
       {/* <Three_D_Cube /> */}
-      <Sign_Up/>
+      {/* <Sign_Up/> */}
+      <BoardForDinosour/>
     </div>
   );
 }
---
+++ client/src/components/Dinosaur/Board.tsx
@@ -0,0 +1,9 @@
+export const BoardForDinosour = () => {
+    return (
+        <div className="w-full h-[400px] border border-[#987] relative p-4">
+            <div className="w-[20px] h-[20px] bg-blue-300 absolute bottom-0 right-10">
+
+            </div>
+        </div>
+    )
+}
